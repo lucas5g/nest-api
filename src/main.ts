f5276b77b6ca6bf6fde19a4c229b128af5914850
lucas.assuncao <lucas.assuncao@defensoria.mg.def.br>
2024-02-06 15:20:01 -0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.enableCors()
+
   await app.listen(3000);
 
   Logger.log('http://localhost:3000');
